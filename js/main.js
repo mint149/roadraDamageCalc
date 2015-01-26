1ac33519209ec6ac6adf96406e1c7db6c8712d84
skylove.exe@gmail.com <skylove.exe@gmail.com>
2015-01-26 16:54:32 +0900

    チェックボックスが動作していなかった問題を修正、レイアウト変更。

--- js/main.js
+++ js/main.js
@@ -11,10 +11,27 @@
 　＋[基礎攻撃力×0.01×0～9の自然数]
 
 ※基礎攻撃力＝ユニット攻撃力(アサルト系スキルによる増加分は含まない)＋基礎攻撃力上昇スキル
+
+{
+	a = ( [基礎攻撃力^0.95]＋[基礎攻撃力×アサルト系スキル×0.01]＋[特化ダメージスキル] )
+　a×[全力の一撃 : YES＝2 , NO＝1]
+　＋[反属性 : YES＝ b=(100＋アキュート系スキル)×基礎攻撃力^0.46 , NO＝0]
+　＋c=[属性補正×(300+エレメント系スキル) ]
+　－[敵防御値] }
+　×d[ (コンボ補正値+チェイン系スキル)×0.01]
+　＋[基礎攻撃力×0.01×0～9の自然数]
+
 */
+
+var calcedAtk;
+var calcedWeak;
+var calcedElement;
+var calcedChain;
 
 var atk = new Array(6);
 var job = new Array(6);
+var panels = new Array(8);
+var jobPanelNum = new Array(4);
 var damagePerUnit = new Array(6);
 var damagePerPanel = new Array(16);
 var assault = 0;
@@ -31,7 +48,8 @@ var chain = [100,60,50,40,40,40,40,30,30,30,30,30,30,30,30,30];
 var chainhosei = 0;
 var rand = 0;
 var totalDamage = 0;
-var panel = [0,0,0,0];
+var cutIn;
+var thunderCutIn;
 
 var damage = [
 	[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
@@ -48,21 +66,34 @@ function calc() {
 		atk[unitNum] = parseInt($("#units").find("input").eq(unitNum).val());
 		job[unitNum] = parseInt($("#units").find("select").eq(unitNum).val());
 	};
-	for (var panelNum = 0; panelNum < panel.length; panelNum++) {
-		panel[panelNum] = parseInt($("#panels").find("select").eq(panelNum).val());
+	jobPanelNum = [0,0,0,0];
+	for (var panelNum = 0; panelNum < panels.length; panelNum++) {
+		panels[panelNum] = parseInt($("#panels").find("select").eq(panelNum).val());
+		jobPanelNum[panels[panelNum]] += 1;
 	};
 	assault = parseInt($("#assault").val());
 	assaultNum = parseInt($("#assaultNum").val());
 	tokkou = parseInt($("#tokkou").val());
-	if($("#zenryoku").attr("checked") == undefined){
-		zenryoku = 1;
-	}else{
+
+	if($("#zenryoku").is(":checked")){
 		zenryoku = 2;
-	}
-	if($("#weak").attr("checked") == undefined){
-		weak = 0;
 	}else{
+		zenryoku = 1;
+	}
+	if($("#weak").is(":checked")){
 		weak = 1;
+	}else{
+		weak = 0;
+	}
+	if($("#cutIn").is(":checked")){
+		cutIn = true;
+	}else{
+		cutIn = false;
+	}
+	if($("#thunderCutIn").is(":checked")){
+		thunderCutIn = true;
+	}else{
+		thunderCutIn = false;
 	}
 	akyuto = parseInt($("#akyuto").val());
 	hosei = parseInt($("#hosei").val());
@@ -75,50 +106,33 @@ function calc() {
 	damagePerUnit = [0,0,0,0,0,0];
 	damagePerPanel = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
 
-	//16パネル(8パネル*ツイン分)のダメージ計算
+	//ダメージ計算
 	for (var unitNum = 0; unitNum < atk.length; unitNum++) {
-		for (var chainNum = 0; chainNum < chain.length; chainNum++) {
-			if (atk[unitNum] == 0) {
-				damage[unitNum][chainNum] = 0;
-			}else{
-				damage[unitNum][chainNum] = ((Math.pow(atk[unitNum],0.95)
-				 + atk[unitNum] * assault * 0.01
-				 + tokkou
-				 + assaultNum)
-				* zenryoku
-				+ weak * (100 + akyuto) * Math.pow(atk[unitNum],0.46)
-				+ hosei * (300 + element)
-				- defence)
-				* Math.min(100,(chain[chainNum] + chainhosei)) * 0.01
-				* (100 + damageBairitu) * 0.01
-				+ atk[unitNum] * 0.01 * rand;
+		for (var panelNum = 0; panelNum < jobPanelNum[job[unitNum]]; panelNum++) {
+			//計算式の各変数を求める
+			calcedAtk = Math.pow(atk[unitNum] + assaultNum, 0.95) + atk[unitNum] * assault * 0.01 + tokkou;
+			calcedWeak = weak * (100 + akyuto) * Math.pow(atk[unitNum],0.46);
+			calcedElement = hosei * (300 + element);
+			calcedChain = Math.min(100,(chain[panelNum] + chainhosei)) * 0.01;
+
+			// カットイン攻撃かつ最後のコンボの場合、チェイン補正を100にする
+			if (cutIn && ((panelNum + 1) == jobPanelNum[job[unitNum]])) {
+				calcedChain = 1;
 			};
-			damagePerUnit[unitNum] += damage[unitNum][chainNum];
-			damagePerPanel[chainNum] += damage[unitNum][chainNum];
+
+			// 稲妻カットイン攻撃かつ最後のコンボの場合、チェイン補正を150にする
+			if (thunderCutIn && ((panelNum + 1) == jobPanelNum[job[unitNum]])) {
+				calcedChain = 1.5;
+			};
+
+			// ユニット攻撃力が0の時はユニットが居ないとみなす
+			if (atk[unitNum] != 0) {
+				//ダメージ計算式に代入
+				totalDamage += (calcedAtk * zenryoku + calcedWeak + calcedElement - defence) * calcedChain * (100 + damageBairitu) * 0.01 + atk[unitNum] * 0.01 * rand;
+			};
 		};
 	};
 
-	//表に結果表示
-	for (var i = 0; i < atk.length; i++) {
-		for (var j = 0; j < chain.length; j++) {
-			$("#damageTable").find("tr").eq(i+1).find("td").eq(j+1).text(parseInt(damage[i][j]));
-		};
-		$("#damageTable").find("tr").eq(i+1).find("td").eq(17).text(parseInt(damagePerUnit[i]));
-	};
-	for (var i = 0; i < chain.length; i++) {
-		$("#damageTable").find("tr").eq(7).find("td").eq(i+1).text(parseInt(damagePerPanel[i]));
-	};
-
-	//パネル何枚でどれくらいダメージが出るか表示
-	for (var i = 0; i < atk.length; i++) {
-		for (var j = 0; j < chain.length; j++) {
-			if(j < panel[job[i]]){
-				totalDamage += damage[i][j];
-				$("#damageTable").find("tr").eq(i+1).find("td").eq(j+1).css("background-color", "#ddddff");
-			}else{
-				$("#damageTable").find("tr").eq(i+1).find("td").eq(j+1).css("background-color", "#ffffff");
-			}
-		}
-	}
+	//結果表示
 	$("#damage").text(parseInt(totalDamage));
 }
